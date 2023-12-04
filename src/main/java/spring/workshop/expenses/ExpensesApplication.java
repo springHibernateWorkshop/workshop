c0fb21d289e79c866bb5a7a4e3ff7c7961d38f5f
springHibernateWorkshop <152858175+springHibernateWorkshop@users.noreply.github.com>
2023-12-04 11:50:12 +0100

    initial commit

---
+++ src/main/java/spring/workshop/expenses/ExpensesApplication.java
@@ -0,0 +1,22 @@
+package spring.workshop.expenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesApplication.class, args);
+	}
+
+	@GetMapping("/dzien_dobry")
+    public String hello(@RequestParam(value = "imie", defaultValue = "World") String name) {
+      	return String.format("Hello11 %s!", name);
+    }
+
+}
