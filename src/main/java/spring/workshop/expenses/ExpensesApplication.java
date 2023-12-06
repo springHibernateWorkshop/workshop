3f66f511496d8031f07a45876edbd9fffed8669b
springHibernateWorkshop <152858175+springHibernateWorkshop@users.noreply.github.com>
2023-12-06 16:25:52 +0100

    Changes needed for deployment

--- src/main/java/spring/workshop/expenses/ExpensesApplication.java
+++ src/main/java/spring/workshop/expenses/ExpensesApplication.java
@@ -2,13 +2,20 @@ package spring.workshop.expenses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class ExpensesApplication {
+public class ExpensesApplication extends SpringBootServletInitializer {
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ExpensesApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpensesApplication.class, args);
