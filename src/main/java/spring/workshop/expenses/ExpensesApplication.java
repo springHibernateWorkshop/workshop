afde9df615312c095989835bdb6c4e081fdac272
Bartosz Minartowicz <bartosz.minartowicz@capgemini.com>
2023-12-08 16:25:09 +0100

    Eine Datei pro RestController

--- src/main/java/spring/workshop/expenses/ExpensesApplication.java
+++ src/main/java/spring/workshop/expenses/ExpensesApplication.java
@@ -4,12 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ExpensesApplication extends SpringBootServletInitializer {
 
 	@Override
@@ -20,10 +16,5 @@ public class ExpensesApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(ExpensesApplication.class, args);
 	}
-
-	@GetMapping("/dobry_wieczor_kochanie")
-    public String hello(@RequestParam(value = "imie", defaultValue = "World") String name) {
-      	return String.format("Hello14 %s!", name);
-    }
 
 }
