8cee4562fdcbd2a18ae39baf5d1d95b8b99c3f85
Victoria Marano <victoria.marano@capgemini.com>
2023-12-12 15:34:04 +0100

    connect to mysql db instead of oracle, split properties in 2 test and prod

--- src/test/java/spring/workshop/expenses/ExpensesApplicationTests.java
+++ src/test/java/spring/workshop/expenses/ExpensesApplicationTests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -17,10 +18,17 @@ class ExpensesApplicationTests {
 	void contextLoads() {
 	}
 
+	@Value("${example.propertyCheck}")
+	private String propertyCheck;
+
 	@Test
 	void testMyTableRowCount() {
 		int actualRowCount = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM USER_TAB", Integer.class);
 		assertEquals(5, actualRowCount);
 	}
 
+	@Test
+	public void shouldTestPropertyOverrideProdProperty() {
+		assertEquals("test", propertyCheck);
+	}
 }
