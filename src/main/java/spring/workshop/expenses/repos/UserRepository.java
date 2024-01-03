b7c279a0ef050d0dda1b6f82aa0fa2f1bb9aeb88
Raffael Schwan <raffael.schwan@capgemini.com>
2024-01-03 14:05:01 +0100

    addind methods to get user by id and by name, refactoring different methods

--- src/main/java/spring/workshop/expenses/repos/UserRepository.java
+++ src/main/java/spring/workshop/expenses/repos/UserRepository.java
@@ -1,14 +1,22 @@
 package spring.workshop.expenses.repos;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import jakarta.transaction.Transactional;
 import spring.workshop.expenses.entities.User;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository  extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
-    User findByName(String name);
+    Optional<User> findByName(String name);
+
+    Optional<User> findById(Integer id);
+
+    @Transactional
+    void deleteByName(String name);
 
 }
