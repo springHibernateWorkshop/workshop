e0cb83eb22ce98d3a2ad726b4cf22a25b5a6c382
Krzysztof Kotara <krzysztof.kotara@capgemini.com>
2024-01-11 07:30:34 +0100

    Add standard Expensesproperties

---
+++ src/main/java/spring/workshop/expenses/entities/Expences.java
@@ -0,0 +1,88 @@
+package spring.workshop.expenses.entities;
+
+import java.sql.Date;
+
+import io.micrometer.common.lang.NonNull;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="expence_tab")
+public class Expences {
+
+  @Id
+  @Column(name="expence_id")
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Integer id;
+
+  @Column
+  @NonNull
+  private String name;
+  private Float total;
+  private Date date;
+  private Integer categoryId;
+  private Integer shopId;
+  private Integer userId;
+  private String note;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Float getTotal() {
+        return total;
+    }
+
+    public void setTotal(Float total) {
+        this.total = total;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Integer getShopId() {
+        return shopId;
+    }
+
+    public void setShopId(Integer shopId) {
+        this.shopId = shopId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+    
+}
