b59f01773ec3f63424299561ef93738ac6535e3f
Raffael Schwan <raffael.schwan@capgemini.com>
2024-01-15 13:22:20 +0100

    change urls, refactor testUpdateUser

--- src/test/java/spring/workshop/expenses/integration/User/UserControllerIntegrationTests.java
+++ src/test/java/spring/workshop/expenses/integration/User/UserControllerIntegrationTests.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -17,15 +16,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import spring.workshop.expenses.entities.User;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class UserControllerIntegrationTests {
-
-    @LocalServerPort
-    private int port;
 
     @Autowired
     private TestRestTemplate restTemplate;
@@ -41,7 +39,7 @@ public class UserControllerIntegrationTests {
         HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, requestHeader);
 
         // URL for adding an user
-        String url = "http://localhost:" + port + "/user/add";
+        String url = "/user/add";
 
         // Send a POST request to add the user
         ResponseEntity<User> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, User.class);
@@ -65,7 +63,7 @@ public class UserControllerIntegrationTests {
         HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, requestHeader);
 
         // URL for deleting an user
-        String url = "http://localhost:" + port + "/user/delete";
+        String url = "/user/delete";
 
         // Send a DELETE request to delete the user
         ResponseEntity<Boolean> response = restTemplate.exchange(url, HttpMethod.DELETE, requestEntity, Boolean.class);
@@ -84,11 +82,10 @@ public class UserControllerIntegrationTests {
         // Constructing the request body with the user to be updated
         HttpHeaders requestHeader = new HttpHeaders();
         requestHeader.setContentType(MediaType.APPLICATION_JSON);
-        String requestBody = "{\"id\": 300, \"name\": \"Test2\"}";
+        String requestBody = new ObjectMapper().writeValueAsString(new User(300, "Test2"));
         HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, requestHeader);
 
-        // URL for deleting an user
-        String url = "http://localhost:" + port + "/user/update";
+        String url = "/user/update";
 
         // Send a DELETE request to delete the user
         ResponseEntity<User> response = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, User.class);
@@ -105,7 +102,7 @@ public class UserControllerIntegrationTests {
     public void testGetAllUsers() throws Exception {
 
         // URL for retrieving all users
-        String url = "http://localhost:" + port + "/user/get_all";
+        String url = "/user/get_all";
 
         // Send a GET request to retrieve all users
         ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
@@ -121,7 +118,7 @@ public class UserControllerIntegrationTests {
     public void testGetUserById() throws Exception {
 
         // URL for retrieving the user by ID
-        String url = "http://localhost:" + port + "/user/get_by_id?id=100";
+        String url = "/user/get_by_id?id=100";
 
         // Send a GET request to retrieve the user by ID
         ResponseEntity<User> response = restTemplate.getForEntity(url, User.class);
@@ -137,7 +134,7 @@ public class UserControllerIntegrationTests {
     public void testGetUserByName() throws Exception {
 
         // URL for retrieving the user by name
-        String url = "http://localhost:" + port + "/user/get_by_name?name=Victoria";
+        String url = "/user/get_by_name?name=Victoria";
 
         // Send a GET request to retrieve the user by name
         ResponseEntity<User> response = restTemplate.getForEntity(url, User.class);
