ed115739713404558de49702a7c42a07be952b6e
Manh Ton Nguyen <manh-ton.nguyen@capgemini.com>
2024-01-15 16:08:19 +0100

    add tess for Aspect

---
+++ src/test/java/spring/workshop/expenses/aspect/PerformanceLoggingAspectTest.java
@@ -0,0 +1,54 @@
+package spring.workshop.expenses.aspect;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+import spring.workshop.expenses.services.ShopServiceImpl;
+
+@SpringBootTest()
+public class PerformanceLoggingAspectTest {
+
+    private ListAppender<ILoggingEvent> listAppender;
+
+    @Autowired
+    private ShopServiceImpl shopService;
+
+    @Autowired
+    private PerformanceLoggingAspect performanceLoggingAspect;
+
+    @BeforeEach
+    public void setup() {
+        Logger logger = (Logger) LoggerFactory.getLogger(PerformanceLoggingAspect.class);
+        listAppender = new ListAppender<>();
+        listAppender.start();
+        logger.addAppender(listAppender);
+    }
+
+    @AfterEach
+    public void teardown() {
+        listAppender.stop();
+        listAppender.list.clear();
+    }
+
+    @Test
+    public void shouldPerformanceLoggingAspect() {
+
+        shopService.getAllShops();
+
+        ILoggingEvent event = listAppender.list.get(0);
+
+        assertEquals(true, event.getMessage().contains("Execution time of"));
+        assertEquals(true, event.getMessage().contains("ShopServiceImpl.getAllShops"));
+
+    }
+
+}
