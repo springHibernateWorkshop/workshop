21cf4887559eb2076f22be3f25fbebf0a31653af
akopacz <agnieszka.kopacz@capgemini.com>
2024-02-02 09:15:13 +0100

    act 3

--- src/main/java/spring/actuator/endpoints/CustomAgaEndpoint.java
+++ src/main/java/spring/actuator/endpoints/CustomAgaEndpoint.java
@@ -3,15 +3,15 @@ import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 //implemented as POC
 
-@Endpoint(id="aga")
+@RequestMapping("aga")
 @Component
 public class CustomAgaEndpoint{
     
-      @ReadOperation
       @Bean
       public String HiFromActuator(){
         return "Hi from endpoint created in Actuator";
