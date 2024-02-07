a86813909720e552291f5169592e7c11de87e2af
akopacz <agnieszka.kopacz@capgemini.com>
2024-02-07 12:01:06 +0100

    getSuperiorById etc zugewiesen

--- src/main/java/spring/workshop/expenses/repos/SuperiorRepository.java
+++ src/main/java/spring/workshop/expenses/repos/SuperiorRepository.java
@@ -1,9 +1,11 @@
 package spring.workshop.expenses.repos;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import spring.workshop.expenses.entities.Superior;
 
 public interface SuperiorRepository extends JpaRepository<Superior, Long>{
-    
+  Optional<Superior> findById(Long id);  
     
 }
