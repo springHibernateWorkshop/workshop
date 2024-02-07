78bcc01459b144e5bf7cce1cddf2f0ff19bb1eb4
Raffael Schwan <raffael.schwan@capgemini.com>
2024-02-07 14:43:03 +0100

    use superior service in employee controller

--- src/main/java/spring/workshop/expenses/controllers/EmployeeController.java
+++ src/main/java/spring/workshop/expenses/controllers/EmployeeController.java
@@ -1,4 +1,4 @@
-package spring.workshop.expenses.rest;
+package spring.workshop.expenses.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,42 +11,35 @@ import org.springframework.web.bind.annotation.RestController;
 
 import spring.workshop.expenses.entities.Employee;
 import spring.workshop.expenses.entities.Superior;
-// import spring.workshop.expenses.exception.ForbiddenResourceException;
+import spring.workshop.expenses.exceptions.ForbiddenResourceException;
 import spring.workshop.expenses.services.EmployeeService;
-// import spring.workshop.expenses.services.SuperiorService;
+import spring.workshop.expenses.services.SuperiorService;
 
 @RestController
 @RequestMapping(path = "/employees")
 public class EmployeeController {
 
   private EmployeeService employeeService;
+  private SuperiorService superiorService;
 
   @Autowired
-  public EmployeeController(EmployeeService employeeService) {
+  public EmployeeController(EmployeeService employeeService, SuperiorService superiorService) {
     this.employeeService = employeeService;
+    this.superiorService = superiorService;
   }
-
-  // private SuperiorService superiorService;
-
-  // @Autowired
-  // public EmployeeController(SuperiorService superiorService) {
-  // this.superiorService = superiorService;
-  // }
 
   // Method for reassigning an employee
   @PutMapping(path = "/{id}")
   public ResponseEntity<Employee> reassignEmployee(@PathVariable("id") Long employeeId,
       @RequestParam("superior_id") Long superiorId) {
-    // Mock for Superior, change to method for getting superior by superior_id
-    Superior superior = new Superior(superiorId, "Superior");
 
     // Get Superior by superior_id
-    // Superior superior = superiorService.getSuperiorById(superiorId);
+    Superior superior = superiorService.getSuperiorById(superiorId);
 
     // Check if Superior.user_id != Null
-    // if (!superior.getUser().isPresent)
-    // throw new ForbiddenResourceException("User for Superior with id = " +
-    // superiorId + "does not exist.");
+    if (superior.getUser() == null)
+      throw new ForbiddenResourceException("User for Superior with id = " +
+          superiorId + "does not exist.");
 
     // Get Employee by id
     Employee updatedEmployee = employeeService.getEmployeeById(employeeId);
