96617b49b4a07fbcf7b02fc1953c35183d848a7b
Victoria Marano <victoria.marano@capgemini.com>
2024-02-27 16:45:30 +0100

    Update WebSecurityConfig to remove prePostEnabled

--- src/main/java/spring/workshop/expenses/security/WebSecurityConfig.java
+++ src/main/java/spring/workshop/expenses/security/WebSecurityConfig.java
@@ -20,7 +20,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class WebSecurityConfig {
 
 	@Bean
