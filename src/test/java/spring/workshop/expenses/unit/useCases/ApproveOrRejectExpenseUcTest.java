dec3bfb993098f19872a8819b8448321723ae696
akopacz <agnieszka.kopacz@capgemini.com>
2024-03-04 12:33:29 +0100

    Approve Test, Reject Test and Approve Negativ Test

---
+++ src/test/java/spring/workshop/expenses/unit/useCases/ApproveOrRejectExpenseUcTest.java
@@ -0,0 +1,121 @@
+package spring.workshop.expenses.unit.useCases;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+
+import org.h2.command.dml.MergeUsing.When;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import spring.workshop.expenses.entities.Category;
+import spring.workshop.expenses.entities.Employee;
+import spring.workshop.expenses.entities.Expense;
+import spring.workshop.expenses.entities.Shop;
+import spring.workshop.expenses.entities.Superior;
+import spring.workshop.expenses.entities.User;
+import spring.workshop.expenses.enums.ExpenseStatus;
+import spring.workshop.expenses.exceptions.ResourceNotFoundException;
+import spring.workshop.expenses.services.EmployeeService;
+import spring.workshop.expenses.services.ExpenseService;
+import spring.workshop.expenses.services.SuperiorService;
+import spring.workshop.expenses.useCases.ApproveOrRejectExpenseUc;
+import spring.workshop.expenses.useCases.impl.ApproveOrRejectExpenseUcImp;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+public class ApproveOrRejectExpenseUcTest {
+
+    @InjectMocks
+    private ApproveOrRejectExpenseUc sut = new ApproveOrRejectExpenseUcImp();
+    
+    @Mock
+    private SuperiorService superiorServiceMock;
+
+    @Mock
+    private ExpenseService expenseServiceMock;
+
+      @Mock
+    private EmployeeService employeeServiceMock;
+
+    @Test
+    public void approveExpenseTest(){
+        Superior superior=new Superior(2L, "Superior", new User(2L, "User"));
+        Expense expense=new Expense(999L, 999.99f, LocalDate.of(1994, 10, 1), new Category(100L),
+                                new Shop(100L),
+                                new Employee(100L, "Test", new User("Test"), superior),
+                                "example_test_note_1");
+
+        expense.setStatus(ExpenseStatus.PENDING);                        
+
+        Employee employee= expense.getEmployee();
+
+        when(expenseServiceMock.getExpenseById(any())).thenReturn(expense);
+        when(employeeServiceMock.getEmployeeById(any())).thenReturn(employee);
+        when(superiorServiceMock.getSuperiorById(any())).thenReturn(superior);
+        when(expenseServiceMock.updateExpense(any())).thenReturn(expense);
+
+        Expense approvedExpense = sut.approveOrRejectExpense(expense.getId(), superior.getId(), ExpenseStatus.APPROVED, "example_approve_note_1");
+
+        assertNotNull(approvedExpense);
+        assertEquals(ExpenseStatus.APPROVED, approvedExpense.getStatus());
+        assertEquals("example_approve_note_1", approvedExpense.getNote());
+
+
+    }
+
+    @Test
+    public void rejectExpenseTest(){
+        Superior superior=new Superior(2L, "Superior", new User(2L, "User"));
+        Expense expense=new Expense(999L, 999.99f, LocalDate.of(1994, 10, 1), new Category(100L),
+                                new Shop(100L),
+                                new Employee(100L, "Test", new User("Test"), superior),
+                                "example_test_note_1");
+
+        expense.setStatus(ExpenseStatus.PENDING);                        
+
+        Employee employee= expense.getEmployee();
+
+        when(expenseServiceMock.getExpenseById(any())).thenReturn(expense);
+        when(employeeServiceMock.getEmployeeById(any())).thenReturn(employee);
+        when(superiorServiceMock.getSuperiorById(any())).thenReturn(superior);
+        when(expenseServiceMock.updateExpense(any())).thenReturn(expense);
+
+        Expense approvedExpense = sut.approveOrRejectExpense(expense.getId(), superior.getId(), ExpenseStatus.REJECTED, "example_reject_note_1");
+
+        assertNotNull(approvedExpense);
+        assertEquals(ExpenseStatus.REJECTED, approvedExpense.getStatus());
+        assertEquals("example_reject_note_1", approvedExpense.getNote());
+    }
+
+    @Test
+
+    public void acceptExpenseNichtRichtigerSuperiorTest(){
+        Superior superior=new Superior(2L, "Superior", new User(2L, "User"));
+        
+        Expense expense=new Expense(999L, 999.99f, LocalDate.of(1994, 10, 1), new Category(100L),
+                                new Shop(100L),
+                                new Employee(100L, "Test", new User("Test"), superior),
+                                "example_test_note_1");
+
+        expense.setStatus(ExpenseStatus.PENDING);                        
+
+        Employee employee= expense.getEmployee();
+
+        when(expenseServiceMock.getExpenseById(any())).thenReturn(expense);
+        when(employeeServiceMock.getEmployeeById(any())).thenReturn(employee);
+        when(superiorServiceMock.getSuperiorById(any())).thenReturn(superior);
+        when(expenseServiceMock.updateExpense(any())).thenReturn(expense);
+
+        Exception exception = assertThrows(ResourceNotFoundException.class,() -> sut.approveOrRejectExpense(expense.getId(), 3L, ExpenseStatus.REJECTED, "example_reject_note_1"));
+        assertEquals("Superior cannot approve this Expense", exception.getMessage());
+    }
+}
