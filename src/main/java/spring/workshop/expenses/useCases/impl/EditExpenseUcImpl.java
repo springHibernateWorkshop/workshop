9b1590166531ea463b399b2a1993b3e005d8ec13
CORP\bminarto <bartosz.minartowicz@capgemini.com>
2024-03-26 16:09:37 +0100

    ExpenseServiceImpl modify updateExpense method

--- src/main/java/spring/workshop/expenses/useCases/impl/EditExpenseUcImpl.java
+++ src/main/java/spring/workshop/expenses/useCases/impl/EditExpenseUcImpl.java
@@ -2,6 +2,7 @@ package spring.workshop.expenses.useCases.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Component;
 
 import spring.workshop.expenses.entities.Employee;
 import spring.workshop.expenses.entities.Expense;
@@ -13,6 +14,7 @@ import spring.workshop.expenses.services.EmployeeService;
 import spring.workshop.expenses.services.ExpenseService;
 import spring.workshop.expenses.useCases.EditExpenseUc;
 
+@Component
 public class EditExpenseUcImpl implements EditExpenseUc {
 
     @Autowired
@@ -23,12 +25,12 @@ public class EditExpenseUcImpl implements EditExpenseUc {
 
     @Override
     @PreAuthorize("hasAuthority('EDIT_EXPENSES')")
-    public Expense editExpense(User user, Expense expense) {
+    public Expense editExpense(User user, Expense expenseNew) {
         // Get Employee by User
         Employee employee = employeeService.getEmployeeByUser(user);
 
         // Get Expense by expense_id
-        Expense expenseOld = expenseService.getExpenseById(expense.getId());
+        Expense expenseOld = expenseService.getExpenseById(expenseNew.getId());
 
         // Check if Expense.employee_id = employee_id
         if (!expenseOld.getEmployee().getId().equals(employee.getId()))
@@ -37,8 +39,8 @@ public class EditExpenseUcImpl implements EditExpenseUc {
         // Check if Expense.status in ('INITIAL')
         if (!expenseOld.getStatus().equals(ExpenseStatus.INITIAL))
             throw new ForbiddenResourceException(
-                    "Expense status needs to be INITIAL (Current status: " + expense.getStatus() + ").");
+                    "Expense status needs to be INITIAL (Current status: " + expenseNew.getStatus() + ").");
 
-        return expenseService.updateExpense(expense);
+        return expenseService.updateExpense(expenseNew);
     }
 }
