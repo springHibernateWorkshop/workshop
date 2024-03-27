7d7304bbd6ea83f60e6a77a30887f61bae0037cb
Victoria Marano <victoria.marano@capgemini.com>
2024-03-27 13:37:45 +0100

    clean shop controller

--- src/main/java/spring/workshop/expenses/controllers/ShopController.java
+++ src/main/java/spring/workshop/expenses/controllers/ShopController.java
@@ -6,12 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import spring.workshop.expenses.entities.Shop;
@@ -24,12 +20,6 @@ public class ShopController {
     @Autowired
     private ShopService shopService;
 
-    @PostMapping
-    public ResponseEntity<Shop> addNewShop(@RequestBody Shop shop) {
-
-        return new ResponseEntity<>(shopService.addNewShop(shop), HttpStatus.CREATED);
-    }
-
     @GetMapping
     public ResponseEntity<List<Shop>> getAllShops() {
         return new ResponseEntity<>(shopService.getAllShops(), HttpStatus.OK);
@@ -39,15 +29,4 @@ public class ShopController {
     public ResponseEntity<Shop> getShop(@PathVariable Long id) {
         return new ResponseEntity<>(shopService.getShop(id), HttpStatus.OK);
     }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Shop> updateShop(@RequestBody Shop room, @PathVariable Long id) {
-        return new ResponseEntity<>(shopService.updateShop(room, id), HttpStatus.OK);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Shop> deleteShop(@PathVariable Long id) {
-        return new ResponseEntity<>(shopService.deleteShop(id), HttpStatus.OK);
-    }
-
 }
